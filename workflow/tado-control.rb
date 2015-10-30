397cb05a64716e3b49224ec74b797c55e109aae7
Raphael Doehring <raphael.doehring@vasco.com>
2015-10-30 21:23:04 +0100

    Requiring tado gem.

--- workflow/tado-control.rb
+++ workflow/tado-control.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+# this fixes some encoding issues running this on OS X 10.11
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require_relative "bundle/bundler/setup"
-require "alfred"
+require_relative 'bundle/bundler/setup'
+require 'alfred'
+require 'tado'
 
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
 
 
-  # add an arbitrary feedback
+  tado = Tado.new(TADO_USERNAME, TADO_PASSWORD)
+
   fb.add_item({
     :uid      => ""                     ,
     :title    => "Hello"          ,
